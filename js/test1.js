c843b77756680fe3968cbe41039ce42420ac1dd1
scroacher <scroacher@gmail.com>
2016-03-25 02:15:14 -0400

    Claned up JS and CSS
    Claned up JS and CSS -

--- js/test1.js
+++ js/test1.js
@@ -1,1 +1,17 @@
-angular.module('example', ['FullpageScroll']);
+function scrollToAnchor(aid){
+    var aTag = $("a[name='"+ aid +"']");
+    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+}
+
+$("#link").click(function() {
+   scrollToAnchor('ph2');
+});
+
+function scrollToAnchor2(aid){
+    var aTag = $("a[name='"+ aid +"']");
+    $('html,body').animate({scrollTop: aTag.offset().top},'slow');
+}
+
+$("#link2").click(function() {
+   scrollToAnchor('ph1');
+});
